7b068a6ce91c28f60693eb15df130eead6ff8c1a
Xxxz <drxxz1992@hotmail.com>
2017-09-05 19:19:19 +0800

    剑指Offer的Java练习

---
+++ P28Anagram.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class P28Anagram{
+	
+	private static char[] charArray;
+	private static int length;    
+
+	public static void doAnagram(int len){
+		
+		if(len == 1){
+			System.out.println(Arrays.toString(charArray));     // basecase
+		}else{
+			for(int i=0;i<len;i++){
+				swap(i,len-1);	
+				doAnagram(len-1); 
+				swap(i,len-1);
+			}
+		}
+	} 
+	
+	private static void swap(int i, int j){
+		char temp;
+		temp = charArray[i];
+		charArray[i] = charArray[j];  
+		charArray[j] = temp;
+	}
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("input word: ");
+		String s = sc.nextLine();  
+		
+		charArray = new char[100];
+		length = s.length();
+		charArray = s.toCharArray();
+		
+		doAnagram(length);
+	}
+}
